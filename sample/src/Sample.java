1202fdd1f214890ccfc16b3fc390cf435bf513c2
taemu <taemu@taemu-15>
2012-10-06 10:36:31 +0900

    seperate wolf class

--- sample/src/Sample.java
+++ sample/src/Sample.java
@@ -25,27 +25,3 @@ class Animal{
 
 
 
-class Wolf extends Animal {
-	
-	private String name;
-	
-	public Wolf() {
-		name = "wolf";
-	}
-	
-	public void run() {
-		System.out.println(name + " running");
-	}
-	
-	public void hunt() {
-		System.out.println(name + " hunting");
-	}
-
-	public void barking() {
-		System.out.println(name + " ah wooool");
-	}
-	
-	public void Eat(String foodName) {
-		System.out.println(name + " eating " + foodName );
-	}
-};
